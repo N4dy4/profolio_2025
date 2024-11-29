1efda1ab17008c8372f1ca3a57f6dc2e76037531
N4dy4 <nadya741@icloud.com>
2024-11-29 18:18:19 -0500

    starting project

---
+++ src/app/app/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "./Components/Heading";
+
+function App(){
+    return <div>
+        <Heading />
+    </div>
+}
+
+export default App
---
+++ src/app/app/Components/Heading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = () => {
+    return <h1 className="Hello My Name is Nadya">Heading</h1>;
+};
+
+export default Heading;
